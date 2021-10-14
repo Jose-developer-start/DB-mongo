385c66d0be4560aa5be34c04625a95887f0debd4
José Deodanes <60304928+Jose-developer-start@users.noreply.github.com>
2021-10-14 09:32:55 -0600

    Update consultas.js

--- lookup/consultas.js
+++ lookup/consultas.js
@@ -30,3 +30,6 @@ db.clientes.aggregate(
 ).pretty()
 
 db.clientes.aggregate([{$match: {cod_cliente:{$in: ["00016","00022","00025"]}}},{$lookup: {from:"facturas",localField: "cod_cliente",foreignField:"cod_cliente",as:"factura"}},{$unwind: "$factura"},{$lookup: {from: "productos",localField: "cod_producto",foreignField:"factura.cod_producto",as:"producto"}},{$unwind: "$producto"},{$limit: 1}]).pretty()
+
+
+db.facturas.aggregate([{$lookup:{from:"clientes",localField: "cod_cliente",foreignField: "cod_cliente", as:"clientes"}},{$lookup:{from:"productos",localField:"cod_producto",foreignField:"cod_producto",as: "productos"}},{$project:{"cod_clientes":0,"cod_producto":0,"clientes._id":0,"productos._id":0}},{$limit:1}]).pretty()
